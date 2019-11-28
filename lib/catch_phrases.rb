0f254259bb43f13406b89a14d5e874791678adb3
Theo Carney <theoluyi@gmail.com>
2019-11-28 17:54:06 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -10,11 +10,11 @@ end
 
 
 
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
-end
+# def mario
+#   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+#   puts phrase
+# end
 
-def toadstool
-  puts status
-end
+# def toadstool
+#   puts status
+# end
